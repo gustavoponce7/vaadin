ec06a4e311aaf8fb56143fc7af7d462ceecb439d
Gustavo Ponce <gustavo_ponce@epam.com>
2019-01-26 20:00:31 +0300

    first commit

---
+++ src/test/java/com/gpch/vaadin/VaadinApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gpch.vaadin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaadinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
